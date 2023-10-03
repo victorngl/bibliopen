e745a83ce8ba1173a93b6e6781d80f069f28a0cc
Joao Victor Costa <victor_ngl2@hotmail.com>
2023-10-03 11:07:53 -0300

    [UPDATE] Criacao de um hook para buscar livro por ISBN

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -2,11 +2,15 @@
 
 import { ChangeEvent, FC, useState } from "react";
 
+import useGetInfoByISBN from "@/hooks/useGetBookByISBN";
+
 interface BookFormProps {
     book?: Book,
 }
 
 const BookForm: FC<BookFormProps> = ({ book }) => {
+    const { getBookByISBN } = useGetInfoByISBN();
+
     const [bookForm, setBookForm] = useState<Book>({
         title: '',
         author: '',
@@ -26,27 +30,14 @@ const BookForm: FC<BookFormProps> = ({ book }) => {
     }
 
     const handleSearchISBN = async () => {
-        const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:' + bookForm?.isbn)
-        const responseJson = await response.json()
 
-        if (responseJson.totalItems != 0) {
+        const book: Book | undefined = await getBookByISBN(bookForm.isbn)
 
-            const bookInfo: Book = {
-                title: responseJson.items[0].volumeInfo.title,
-                author: responseJson.items[0].volumeInfo.authors[0],
-                genre: responseJson.items[0].volumeInfo.categories[0],
-                published_year: responseJson.items[0].volumeInfo.publishedDate,
-                available: false,
-                isbn: bookForm.isbn
-            }
-
-
-            setBookForm(bookInfo)
-
+        if (book != undefined) {
+            setBookForm(book)
         }
+
     }
-
-
 
     return (
         <>
@@ -171,29 +162,3 @@ const BookForm: FC<BookFormProps> = ({ book }) => {
 
 export default BookForm;
 
-
-function Form() {
-    return (
-        <div>
-            <form>
-                <p>Titulo</p>
-                <input name="title" type="text" value={bookForm?.title} onChange={(e) => handleFormChange(e)}></input>
-                <p>Autor</p>
-                <input name="author" type="text" value={bookForm?.author} onChange={(e) => handleFormChange(e)}></input>
-                <p>ISBN</p>
-                <input name="isbn" type="text" value={bookForm?.isbn} onChange={(e) => handleFormChange(e)}></input>
-                <p>Ano de Publicação</p>
-                <input name="published_year" type="number" value={bookForm?.published_year} onChange={(e) => handleFormChange(e)}></input>
-                <p>Genero</p>
-                <input name="genre" type="text" value={bookForm?.genre} onChange={(e) => handleFormChange(e)}></input>
-                <p>Disponivel</p>
-                <input name="available" type="boolean" value={bookForm?.available} onChange={(e) => handleFormChange(e)}></input>
-            </form>
-
-            <div>
-                <button className=" " onClick={handleClick}>LOG</button>
-                <button className=" " onClick={handleSearchISBN}>Get byISBN</button>
-            </div>
-        </div>
-    )
-}
---
+++ src/hooks/useGetBookByISBN.ts
@@ -0,0 +1,30 @@
+
+export default function useGetBookByISBN() {
+
+    const getBookByISBN = async (isbn: string) => {
+
+        const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn)
+        const responseJson = await response.json()
+
+        if (responseJson.totalItems != 0) {
+
+            const bookInfo = {
+                title: responseJson.items[0].volumeInfo.title,
+                author: responseJson.items[0].volumeInfo.authors[0],
+                genre: responseJson.items[0].volumeInfo.categories[0],
+                published_year: responseJson.items[0].volumeInfo.publishedDate,
+                available: false,
+                isbn: isbn,
+            } as Book
+
+            return bookInfo;
+        }
+
+        return undefined;
+
+    }
+
+
+    return { getBookByISBN };
+    
+}
